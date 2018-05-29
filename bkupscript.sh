627a6d313fd5c877d5989b1a1676345864d53cae
Vadim Nicolai <nicolai.vadim@gmail.com>
2018-05-29 21:05:33 +0300

    uploadToFtp func added.

--- bkupscript.sh
+++ bkupscript.sh
@@ -5,6 +5,52 @@ SRC=$1
 DST=$2
 GIVENNAME=$3
 
-showhelp(){
+# FTP Login Data
+USERNAME="USERNAME HERE"
+PASSWORD="PASSWORD HERE"
 
+#Remote directory where the backup will be placed
+REMOTEDIR="./"
+
+showhelp(){
+	echo "\n\n############################################"
+	echo "# bkupscript.sh                            #"
+	echo "############################################"
+	echo "\nThis script will backup files/folders into a single compressed file and will store it in the current folder."
+	echo "In order to work, this script needs the following three parameters in the listed order: "
+	echo "\t- The full path for the folder or file you want to backup."
+	echo "\t- The name of the Space where you want to store the backup at."
+	echo "\t- The name for the backup file (timestamp will be added to the beginning of the filename)\n"
+	echo "Example: sh bckupscript.sh ./testdir testSpace backupdata\n"
 }
+
+tarandzip(){
+    echo "\n##### Gathering files #####\n"
+    if tar -czvf $GIVENNAME-$DATETIME.tar.gz $SRC; then
+        echo "\n##### Done gathering files #####\n"
+        return 0
+    else
+        echo "\n##### Failed to gather files #####\n"
+        return 1
+    fi
+}
+
+uploadToFtp(){
+ftp -n -i $SERVER <<EOF
+	user $USERNAME $PASSWORD
+	binary
+	cd $REMOTEDIR
+	mput $GIVENNAME-$DATETIME.tar.gz
+	quit
+EOF
+}
+
+if [ ! -z "$GIVENNAME" ]; then
+    if tarandzip; then
+        uploadToFtp
+    else
+        showhelp
+    fi
+else
+    showhelp
+fi
